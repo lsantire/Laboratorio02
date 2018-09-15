1afc0f6a8caf46d22933f3cbf1488cab651bcb79
lsantire <lsantire@hotmail.com>
2018-09-15 00:22:36 -0300

    NuevoPedidoActivity muy avanzado.

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedidoActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedidoActivity.java
@@ -1,13 +1,161 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.PedidoDetalle;
 
 public class NuevoPedidoActivity extends AppCompatActivity {
+
+    class RequestCode {
+        static final int AGREGAR_PRODUCTO = 100;
+    }
+
+    Bundle extras;
+    Pedido pedido;
+    PedidoRepository repositorioPedidos;
+    ProductoRepository repositorioProductos;
+    String auxiliarDireccion;
+    PedidoDetalle detallePedidoSeleccionado;
+
+    ArrayAdapter<PedidoDetalle> adaptadorDetallePedido;
+
+    RadioGroup optPedidoModoEntrega;
+    RadioButton optPedidoRetira, optPedidoEnviar;
+    EditText editDireccion,editPedidoCorreo;
+    ListView listaProductosPedido;
+    Button btnQuitarProductoPedido, btnAgregarProductoPedido,btnFinalizarPedido;
+    TextView tvCostoTotalPedido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_pedido);
+
+        pedido = new Pedido();
+        repositorioPedidos = new PedidoRepository();
+        repositorioProductos = new ProductoRepository();
+        auxiliarDireccion = new String();
+        adaptadorDetallePedido = new ArrayAdapter<>(NuevoPedidoActivity.this, android.R.layout.simple_list_item_single_choice, pedido.getDetalle());
+        extras = getIntent().getExtras();
+
+        optPedidoModoEntrega = (RadioGroup) findViewById(R.id.optPedidoModoEntrega);
+        optPedidoEnviar = (RadioButton) findViewById(R.id.optPedidoEnviar);
+        optPedidoRetira = (RadioButton) findViewById(R.id.optPedidoRetira);
+        editDireccion = (EditText) findViewById(R.id.editDireccion);
+        listaProductosPedido = (ListView) findViewById(R.id.listProductosPedido);
+        btnAgregarProductoPedido = (Button) findViewById(R.id.btnAgregarProductoPedido);
+        btnQuitarProductoPedido = (Button) findViewById(R.id.btnQuitarProductoPedido);
+        tvCostoTotalPedido = (TextView) findViewById(R.id.tvCostoTotalPedido);
+        btnFinalizarPedido = (Button) findViewById(R.id.btnFinalizarPedido);
+        editPedidoCorreo = (EditText) findViewById(R.id.editPedidoCorreo);
+
+        tvCostoTotalPedido.setText(R.string.costoTotal+String.format("%.2f",pedido.total()));
+        listaProductosPedido.setAdapter(adaptadorDetallePedido);
+
+        if(getIntent().hasExtra("cantidad") && getIntent().hasExtra("idProducto")) onActivityResult(RequestCode.AGREGAR_PRODUCTO,Activity.RESULT_OK,getIntent());
+
+        optPedidoModoEntrega.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == optPedidoEnviar.getId()) {
+                    editDireccion.setEnabled(true);
+                    editDireccion.setText(auxiliarDireccion);
+                } else {
+                    editDireccion.setText("");
+                    editDireccion.setEnabled(false);
+                }
+            }
+        });
+
+        /*listaProductosPedido.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                btnQuitarProductoPedido.setEnabled(true);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                btnQuitarProductoPedido.setEnabled(false);
+            }
+        });*/
+
+        listaProductosPedido.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                btnQuitarProductoPedido.setEnabled(true);
+                detallePedidoSeleccionado = (PedidoDetalle) parent.getItemAtPosition(position);
+            }
+        });
+
+        btnQuitarProductoPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pedido.quitarDetalle(detallePedidoSeleccionado);
+                adaptadorDetallePedido.notifyDataSetChanged();
+                tvCostoTotalPedido.setText(R.string.costoTotal+String.format("$%.2f",pedido.total()));
+            }
+        });
+
+        btnAgregarProductoPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NuevoPedidoActivity.this, VerProductosActivity.class);
+                i.putExtra("fromNuevoPedido",0);
+                startActivityForResult(i, RequestCode.AGREGAR_PRODUCTO);
+            }
+        });
+
+        btnFinalizarPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(readyToFinish())
+                {
+                    pedido.setDireccionEnvio(editDireccion.getText().toString());
+                    pedido.setEstado(Pedido.Estado.REALIZADO);
+                    pedido.setMailContacto(editPedidoCorreo.getText().toString());
+                    pedido.setRetirar(optPedidoRetira.isChecked());
+                    pedido.setFecha();//HACER DESPUES
+                    repositorioPedidos.guardarPedido(pedido);
+                }
+            }
+        });
+
+    }
+
+    private boolean readyToFinish()
+    {
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        int cantidad,idProductoAgregado;
+        switch (requestCode) {
+            case (RequestCode.AGREGAR_PRODUCTO): {
+                if (resultCode == Activity.RESULT_OK) {
+                    cantidad = data.getIntExtra("cantidad",-1);
+                    idProductoAgregado = data.getIntExtra("idProducto",-1);
+                    pedido.agregarDetalle(new PedidoDetalle(cantidad, repositorioProductos.buscarPorId(idProductoAgregado)));
+                    adaptadorDetallePedido.notifyDataSetChanged();
+                    tvCostoTotalPedido.setText(R.string.costoTotal+String.format("%.2f",pedido.total()));
+                }
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/VerProductosActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/VerProductosActivity.java
@@ -1,12 +1,22 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
@@ -14,34 +24,121 @@ import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
 
 public class VerProductosActivity extends AppCompatActivity {
 
-    private Spinner spinnerCategorias;
-    private ListView listProductos;
+    private List<Producto> productos;
     private ArrayAdapter<Producto> adaptadorProductos;
     private ArrayAdapter<Categoria> adaptadorCategorias;
-    private ProductoRepository productos;
+    private ProductoRepository repositorioProductos;
+    private int idProductoSeleccionado;
+
+    View selectedView;
+
+    private Spinner spinnerCategorias;
+    private ListView listProductos;
+    private Button btnAgregarProducto;
+    private EditText editCantidadProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_productos);
 
-        productos =  new ProductoRepository();
+        repositorioProductos =  new ProductoRepository();
 
         spinnerCategorias = (Spinner) findViewById(R.id.spinnerCategorias);
         listProductos = (ListView) findViewById(R.id.listProductos);
+        btnAgregarProducto = (Button) findViewById(R.id.btnAgregarProducto);
+        editCantidadProducto = (EditText) findViewById(R.id.editCantidadProducto);
 
-        adaptadorCategorias = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, productos.getCategorias());
+        adaptadorCategorias = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, repositorioProductos.getCategorias());
         spinnerCategorias.setAdapter(adaptadorCategorias);
 
         spinnerCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                adaptadorProductos = new ArrayAdapter<Producto>(VerProductosActivity.this,android.R.layout.simple_list_item_1, productos.buscarPorCategoria((Categoria) parent.getItemAtPosition(position)));
+                /*productos = repositorioProductos.buscarPorCategoria((Categoria) parent.getItemAtPosition(position));
+                if(listProductos.getAdapter()==null){
+                    adaptadorProductos = new ArrayAdapter<Producto>(VerProductosActivity.this,android.R.layout.simple_list_item_1, productos);
+                    listProductos.setAdapter(adaptadorProductos);
+                }
+                else adaptadorProductos.notifyDataSetChanged();*/
+                btnAgregarProducto.setEnabled(false);
+                editCantidadProducto.setText("");
+                editCantidadProducto.setEnabled(false);
+                idProductoSeleccionado = -1;
+                productos = repositorioProductos.buscarPorCategoria((Categoria) parent.getItemAtPosition(position));
+                adaptadorProductos = new ArrayAdapter<Producto>(VerProductosActivity.this,android.R.layout.simple_list_item_single_choice, productos);
                 listProductos.setAdapter(adaptadorProductos);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                btnAgregarProducto.setEnabled(false);
+                editCantidadProducto.setText("");
+                editCantidadProducto.setEnabled(false);
+            }
+        });
+
+        listProductos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                idProductoSeleccionado = ((Producto) parent.getItemAtPosition(position)).getId();
+                editCantidadProducto.setText("1");
+                editCantidadProducto.setEnabled(true);
+                btnAgregarProducto.setEnabled(true);
+            }
+        });
+
+      /*  listProductos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                editCantidadProducto.setText("1");
+                editCantidadProducto.setEnabled(true);
+                btnAgregarProducto.setEnabled(true);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                btnAgregarProducto.setEnabled(false);
+                editCantidadProducto.setText("");
+                editCantidadProducto.setEnabled(false);
+            }
+        });*/
+
+        btnAgregarProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getIntent().hasExtra("fromNuevoPedido")) {
+                    Intent i = new Intent();
+                    i.putExtra("cantidad", Integer.valueOf(editCantidadProducto.getText().toString()));
+                    i.putExtra("idProducto", idProductoSeleccionado);
+                    setResult(Activity.RESULT_OK, i);
+                    finish();
+                }
+                else {
+                    Intent i = new Intent(VerProductosActivity.this, NuevoPedidoActivity.class);
+                    i.putExtra("cantidad", Integer.valueOf(editCantidadProducto.getText().toString()));
+                    i.putExtra("idProducto", idProductoSeleccionado);
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        });
+
+        editCantidadProducto.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().isEmpty()) btnAgregarProducto.setEnabled(false);
+                else btnAgregarProducto.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
