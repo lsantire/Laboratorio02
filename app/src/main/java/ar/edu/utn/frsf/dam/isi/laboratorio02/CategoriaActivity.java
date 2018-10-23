8ac76b7b5f6c5b4d1adb40f60822828287d5d28e
ElTomaa <tomas_federal@hotmail.com>
2018-10-23 15:38:54 -0300

    Falta hacer las pruebas y verificar que todo ande

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaActivity.java
@@ -5,33 +5,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
 
 public class CategoriaActivity extends AppCompatActivity {
 
-        private EditText txtNombreCategoria;
-        private Button btnCrearCategoria,btnCategoriaVolver;
+    private EditText txtNombreCategoria;
+    private Button btnCrearCategoria, btnCategoriaVolver;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_categoria);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria);
 
-            txtNombreCategoria = findViewById(R.id.txtNombreCategoria);
-            btnCrearCategoria = findViewById(R.id.btnCrearCategoria);
-            btnCategoriaVolver = findViewById(R.id.btnCategoriaVolver);
+        txtNombreCategoria = findViewById(R.id.txtNombreCategoria);
+        btnCrearCategoria = findViewById(R.id.btnCrearCategoria);
+        btnCategoriaVolver = findViewById(R.id.btnCategoriaVolver);
 
-            btnCrearCategoria.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //completar
-                }
-            });
+        btnCrearCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            btnCategoriaVolver.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-        }
+                Runnable rAceptarPedidos = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.currentThread().sleep(1);
+                            txtNombreCategoria.setText("");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+
+                        }
+                        Categoria cat = new Categoria();
+                        cat.setNombre(txtNombreCategoria.getText().toString());
+                        CategoriaRest categoriaRest = new CategoriaRest();
+                        try {
+                            categoriaRest.crearCategoria(cat);
+                            Toast.makeText(CategoriaActivity.this,"Categoria creada!",Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                Thread hiloActualizacionEstadoPedidos = new Thread(rAceptarPedidos);
+                hiloActualizacionEstadoPedidos.start();
+            }
+        });
+
+        btnCategoriaVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
